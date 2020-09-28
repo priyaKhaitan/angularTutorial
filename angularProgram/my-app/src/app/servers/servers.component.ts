5d0278ec5d6edbf7220e5081e400ec53058b718b
Priya Khaitan <priya.khaitan@niit-tech.com>
2020-09-28 11:16:03 +0530

    changes

--- angularProgram/my-app/src/app/servers/servers.component.ts
+++ angularProgram/my-app/src/app/servers/servers.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: '<app-server></app-server>',
+  // tslint:disable-next-line: component-selector
+  selector: '[app-servers]',
+  template: `<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
